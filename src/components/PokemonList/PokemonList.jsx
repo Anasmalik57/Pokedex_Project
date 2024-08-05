34873b58b19fdf7971fe869ed75d28e75df6f5b7
Anasmalik57 <itsanas474@gmail.com>
2024-08-05 15:11:51 +0530

    Pokedex Working fine with previous and next button functionalities

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -7,14 +7,24 @@ function PokemonList() {
   const [pokemonList, setPokemonList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon/";
+  const [pokedex_url, setPokedex_url] = useState(
+    "https://pokeapi.co/api/v2/pokemon/"
+  );
+
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
 
   async function downloadPokemons() {
-    const response = await axios.get(POKEDEX_URL); // this downloads list of 20 pokemons
+
+    setIsLoading(true)
+
+    const response = await axios.get(pokedex_url); // this downloads list of 20 pokemons
 
     const pokemonResults = response.data.results; // We get array of pokemons from result
 
     console.log(response.data);
+    setNextUrl(response.data.next);
+    setPrevUrl(response.data.previous);
 
     // iterating over the array of pokemons and using their url to create an array of promises
     // that will download those 20 pokemons
@@ -47,21 +57,21 @@ function PokemonList() {
 
   useEffect(() => {
     downloadPokemons();
-  }, []);
+  }, [pokedex_url]);
 
   return (
     <div id="pokemonListWrapper">
       {/* <div id="list"> PokemonLists:</div> */}
       <div className="pokemon-wrapper">
         {isLoading
-          ? "Loading.....😄😄😄"
+          ? "Loading....."
           : pokemonList.map((p) => (
               <Pokemon name={p.name} image={p.image} key={p.id} />
             ))}
       </div>
       <div className="controls">
-        <button>Prev</button>
-        <button>Next</button>
+        <button disabled={prevUrl == null} onClick={()=>setPokedex_url(prevUrl)} >Prev</button>
+        <button disabled={nextUrl == null} onClick={()=>setPokedex_url(nextUrl)} >Next</button>
       </div>
     </div>
   );
